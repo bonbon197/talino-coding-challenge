cb32265c5fbab90a268c2b47e1d372f9297b0f0d
bonbon <nepunepubon@gmail.com>
2024-05-27 01:50:29 +0800

    problem set 2

---
+++ problem_set_2/valid_parentheses.rb
@@ -0,0 +1,25 @@
+def valid_parentheses(s)
+    stack = []
+    brackets = {
+        ')' => '(',
+        '}' => '{',
+        ']' => '['
+    }
+
+    s.each_char do |char|
+        if brackets.values.include?(char)
+            stack.push(char)
+        elsif brackets.keys.include?(char)
+            return false if stack.empty? || stack.pop != brackets[char]
+        end
+    end
+
+    stack.empty?
+end
+
+
+puts valid_parentheses("()")  # true
+puts valid_parentheses("()[]{}")  # true
+puts valid_parentheses("(]")  # false
+puts valid_parentheses("([)]")  # false
+puts valid_parentheses("{[]}")  # true
