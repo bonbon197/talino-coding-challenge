ff45e57af4c4f62125f0822a84c60fb4e01ebc50
bonbon <nepunepubon@gmail.com>
2024-05-27 00:32:12 +0800

    added more safeguards for problem set 1

--- problem_set_1/palindrome_pairs.rb
+++ problem_set_1/palindrome_pairs.rb
@@ -1,11 +1,16 @@
 def palindrome_pairs(words)
+    return [] if words.empty?
+    return "one word array" if words.length == 1
+
     pairs = []
 
     words.each_with_index do |word1, i|
+        next if word1.empty?
         words.each_with_index do |word2, j|
+            next if word2.empty?
             next if i == j
 
-            concatenated = word1 + word2
+            concatenated = word1.downcase + word2.downcase
 
             if concatenated == concatenated.reverse
                 pairs << [i, j]
@@ -16,6 +21,8 @@ def palindrome_pairs(words)
     pairs
 end
 
-words = ["code", "edoc", "da", "ad", "abc"]
+words = ["Code", "edoc", "da", "Ad", "abc"]
 result = palindrome_pairs(words)
 puts result.inspect
+
+# should special characters and unicode characters be included in testing?
